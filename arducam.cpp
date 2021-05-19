397c97fb3071c322ed21bcc762e6c8fe7883288c
sunyton <sunytoncxw@gmail.com>
2021-05-19 13:50:17 +0800

    cd

--- arducam.cpp
+++ arducam.cpp
@@ -1,4 +1,4 @@
-// #include "pxt.h"
+#include "pxt.h"
 
 //% color="#ff6600" weight=50 icon="\uf110"
 namespace arducam {
@@ -9,8 +9,8 @@ namespace arducam {
     //% blockId=arducam_init2 block="init2 camera"
     //% group="ok"
     void init12() {
-        // MicroBitI2C i2c(I2C_SDA0, I2C_SCL0);
-        // uint8_t buffer[]={0x2e, 0xdf}; 
-        // i2c.write(0x30,buffer, 2);
+        MicroBitI2C i2c(I2C_SDA0, I2C_SCL0);
+        uint8_t buffer[]={0x2e, 0xdf}; 
+        i2c.write(0x30,buffer, 2);
     }
 }
