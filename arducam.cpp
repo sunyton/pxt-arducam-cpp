26ecdd9d55434c2ee00e4ad86c33f7f2b5355920
sunyton <sunytoncxw@gmail.com>
2021-05-19 16:00:14 +0800

    edit

--- arducam.cpp
+++ arducam.cpp
@@ -10,7 +10,7 @@ namespace arducam {
     //% blockId=arducam_init2 block="init2 camera"
     //% group="ok"
     void init12() {
-        MicroBitI2C i2c(I2C_SDA0, I2C_SCL0);
+        MicroBitI2C i2c = MicroBitI2C(I2C_SDA0, I2C_SCL0);
         uint8_t buffer[]={0x2e, 0xdf}; 
         i2c.write(0x30,buffer, 2);
     }
