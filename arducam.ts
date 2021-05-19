795ed823f7cc387d6c8de6a173f9a72303b9c6be
sunyton <sunytoncxw@gmail.com>
2021-05-19 13:37:31 +0800

    test

--- arducam.ts
+++ arducam.ts
@@ -1,4 +1,9 @@
 //% color="#ff6600" weight=20 icon="\uf085"
 namespace arducam {
-    
+
+    //% blockId=arducam_init block="init camera"
+    //% group="ok"
+    export function begin() {
+        
+    }
 }
