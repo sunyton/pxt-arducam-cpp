518243e896434b7fdf98aa59216c4091f23d640f
sunyton <sunytoncxw@gmail.com>
2021-05-19 15:49:22 +0800

    fd

--- arducam.cpp
+++ arducam.cpp
@@ -11,7 +11,7 @@ namespace arducam {
     //% group="ok"
     void init12() {
         // MicroBitI2C i2c(I2C_SDA0, I2C_SCL0);
-        // uint8_t buffer[]={0x2e, 0xdf}; 
+        uint8_t buffer[]={0x2e, 0xdf}; 
         // i2c.write(0x30,buffer, 2);
     }
 }
