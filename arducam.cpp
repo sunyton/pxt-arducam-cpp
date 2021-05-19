f98c6c9bae0e36db1fa94e5d2a6324d088055c94
sunyton <sunytoncxw@gmail.com>
2021-05-19 13:17:15 +0800

    ff

--- arducam.cpp
+++ arducam.cpp
@@ -3,7 +3,11 @@
 //% color="#ff6600" weight=50 icon="\uf110"
 namespace arducam {
 
+    /**
+    * init 
+    */
     //% blockId=init block="init camera"
+    //% group="ok"
     void init() {
         // MicroBitI2C i2c(I2C_SDA0, I2C_SCL0);
         // uint8_t buffer[]={0x2e, 0xdf}; 
